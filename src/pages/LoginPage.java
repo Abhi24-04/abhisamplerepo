67780d9b4d94b17b3682c8273e2dd716bf4e2e5b
Abhi24-04 <abhi.sadaphal@gmail.com>
2022-12-18 12:51:48 +0530

    files modified/added/deleted

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Login page");
+		String un = "admin";
+		String pwd = "admin";
+		System.out.println("Logged in");
 
 	}
 
